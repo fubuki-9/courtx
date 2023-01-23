b828e62de895e6943537fa1f0292086c89816906
Sourav  Padhi <97221197+heysouravv@users.noreply.github.com>
2023-01-23 13:43:22 +0530

    Update contract.sol

--- smartContract/contract.sol
+++ smartContract/contract.sol
@@ -9,6 +9,12 @@ contract Litigation {
         address party;
         bool accepted;
     }
+    bool public stopped;
+    event EvidenceAdded(address indexed party, bytes32 indexed hash);
+    event EvidenceAccepted(address indexed party, bytes32 indexed hash);
+    event EvidenceDisputed(address indexed party, bytes32 indexed hash);
+    event AccessGranted(address indexed party, bytes32 indexed hash);
+    event EmergencyStop();
 
     constructor() public {
         owner = msg.sender;
@@ -16,17 +22,22 @@ contract Litigation {
 
     function addParty(address party) public {
         require(msg.sender == owner);
+        require(!stopped);
         parties[party] = true;
     }
 
     function addEvidence(bytes32 hash, address party) public {
         require(parties[party] == true);
+        require(!stopped);
         evidences[hash] = Evidence(hash, party, false);
+        emit EvidenceAdded(party, hash);
     }
 
     function acceptEvidence(bytes32 hash) public {
         require(msg.sender == owner);
+        require(!stopped);
         evidences[hash].accepted = true;
+        emit EvidenceAccepted(evidences[hash].party, hash);
     }
 
     function viewEvidence(bytes32 hash) public view returns (bytes32, address, bool) {
@@ -34,13 +45,29 @@ contract Litigation {
     }
 
     function disputeEvidence(bytes32 hash) public {
+        require(msg.sender == owner);
         require(evidences[hash].accepted == true);
+        require(!stopped);
+        emit EvidenceDisputed(evidences[hash].party, hash);
         delete evidences[hash];
-    }
+        }
 
     function grantAccess(address party, bytes32 hash) public {
         require(msg.sender == owner);
         require(evidences[hash].accepted == true);
+        require(!stopped);
+        emit AccessGranted(party, hash);
         evidences[hash].party = party;
+    }
+
+    function emergencyStop() public {
+        require(msg.sender == owner);
+        stopped = true;
+        emit EmergencyStop();
+    }
+
+    function resume() public {
+        require(msg.sender == owner);
+        stopped = false;
     }
 }
